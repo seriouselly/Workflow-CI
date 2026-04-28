f3677f2bde85ddb9fe5b753eceb52e032ebf9824
seriouselly <supriyatinselly@gmail.com>
2026-04-28 21:48:31 +0700

    first commit

---
+++ MLProject/modelling.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import mlflow
+import dagshub
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+# Mengambil kredensial dari "kunci rahasia" GitHub (Secrets)
+dagshub_user = os.getenv("DAGSHUB_USERNAME")
+dagshub_token = os.getenv("DAGSHUB_TOKEN")
+dagshub.init(repo_owner='seriouselly', repo_name='Workflow-CI-V2', mlflow=True)
+
+df = pd.read_csv('dataset_raw/StudentsPerformance.csv')
+X = df.drop(columns=['math score'])
+y = df['math score']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+numeric_features = ['reading score', 'writing score']
+categorical_features = ['gender', 'race/ethnicity', 'parental level of education', 'lunch', 'test preparation course']
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', StandardScaler(), numeric_features),
+        ('cat', OneHotEncoder(drop='first'), categorical_features)
+    ])
+
+X_train_processed = preprocessor.fit_transform(X_train)
+X_test_processed = preprocessor.transform(X_test)
+
+with mlflow.start_run():
+    rf = RandomForestRegressor(n_estimators=50, random_state=42)
+    rf.fit(X_train_processed, y_train)
+    
+    # Mencatat model agar nanti bisa dibungkus oleh Docker
+    mlflow.sklearn.log_model(rf, "model")
+    print("Model berhasil dilatih dan dicatat oleh Robot!")
